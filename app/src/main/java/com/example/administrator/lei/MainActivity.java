c2ba9abed92cd2534cba3477c2a086275b1e48e4
leichen <leichensmile@gmail.com>
2017-08-05 13:45:42 +0800

    大修改

--- app/src/main/java/com/example/administrator/lei/MainActivity.java
+++ app/src/main/java/com/example/administrator/lei/MainActivity.java
@@ -10,10 +10,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int i = 0;
-        int j = 2;
-        int k = 1;
-        int bbb = 0;
 
+
+        int daksdjasjda;
+
+        int dasjdlj;
 
     }
 }
